c9b9dfc31894e5ca05aa90654fa77fee84a0d751
B H An <anbyunghoon0107@gamil.com>
2021-11-07 22:39:12 +0900

    임시 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([  // 이 배열은 TodoList에 props로 전달된다.
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어보기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
